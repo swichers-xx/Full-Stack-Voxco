64159ad49ad53a79c8ef4ebc45d13d0a0d4a48ad
swichers-xx <seanwichers@gmail.com>
2023-07-17 19:30:50 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Index.js
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; 
+import { store } from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
