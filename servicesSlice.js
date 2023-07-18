cad97aa873b0ec45bf46dcd13dd25772ce9604bd
swichers-xx <seanwichers@gmail.com>
2023-07-17 19:36:32 -0600

    Update servicesSlice.js

--- servicesSlice.js
+++ servicesSlice.js
@@ -4,3 +4,27 @@ export const fetchServices = createAsyncThunk('services/fetch', async () => {
   const response = await fetch('/api/services');
   return response.json();
 });
+// Slice
+const servicesSlice = createSlice({
+  extraReducers: {
+    [fetchServices.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [fetchServices.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = 'idle';
+    },
+    [fetchServices.rejected]: (state) => {
+      state.status = 'failed';
+    }
+  }
+})  
+
+// Component
+if (status === 'loading') {
+  return <Loader />
+} else if (status === 'failed') {
+  return <ErrorMsg /> 
+} else {
+  // render services
+}
